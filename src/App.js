25b7a3a6874980d1654dacadf02cf8699e1d4aa6
FernandoMPN <42014973+FernandoMPN@users.noreply.github.com>
2020-03-18 13:02:46 -0300

    Inicio da implementacao da default page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './Styles.css'
+import telefone from './images/phone.png'
+
+import Checklist from './Checklist/Checklist'
+
+const PaginasApp = {
+
+    Home: 1,
+    SobreADoenca: 2,
+    Prevencao: 3,
+    Mapa: 4,
+    FakeNews: 5
+
+}
+
+function MenuItem({nome, action, style}){
+
+    return(
+        <div className="MenuItem" onClick={action} style={style}>
+            <text>{nome}</text>
+        </div>
+    )
+
+}
+
+function Menu({ changePage, currentPage }){
+
+    const customStyle = {fontWeight: "bold"}
+
+    return(
+        <header>
+            <MenuItem nome="Estou com Corona Vírus?" action={() => changePage(PaginasApp.Home)} style={currentPage===PaginasApp.Home? customStyle:null}/>
+            <MenuItem nome="Sobre a doença" action={() => changePage(PaginasApp.SobreADoenca)} style={currentPage===PaginasApp.SobreADoenca? customStyle:null}/>
+            <MenuItem nome="Prevenção" action={() => changePage(PaginasApp.Prevencao)} style={currentPage===PaginasApp.Prevencao? customStyle:null}/>
+            <MenuItem nome="Mapa de UBS" action={() => changePage(PaginasApp.Mapa)} style={currentPage===PaginasApp.Mapa? customStyle:null}/>
+            <MenuItem nome="Fake News" action={() => changePage(PaginasApp.FakeNews)} style={currentPage===PaginasApp.FakeNews? customStyle:null}/>
+        </header>
+    )
+
+}
+
+function Footer(){
+    
+    return(
+        <footer>
+            <div className="DisqueSaude">
+                <img src={telefone} alt="Ícone de telefone"/>
+                <text>Disque Saúde - 136</text>
+            </div>
+
+            <div className="FooterLinks">
+                <MenuItem nome="Sobre a página"/>
+                <MenuItem nome="Aviso legal"/>
+                <MenuItem nome="Links úteis"/>
+            </div>
+
+        </footer>
+    )
+
+}
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+    const [paginaAtual, mudarPagina] = useState(PaginasApp.Home)
+
+    const setPage = () => {
+
+        switch(paginaAtual){
+            case PaginasApp.Home:
+                return <Checklist/>
+            case PaginasApp.SobreADoenca:
+                return //Sobre
+            case PaginasApp.Prevencao:
+                return //Prevencao
+            case PaginasApp.Mapa:
+                return //Mapa
+            case PaginasApp.FakeNews:
+                return //FakeNews
+        }
+
+    }
+
+    return (
+        <div className="App">
+        <Menu changePage={mudarPagina} currentPage={paginaAtual} />
+            <div className="AppArea">
+
+                {setPage()}
+
+            </div>
+        <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Checklist/Checklist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// import './Styles.css'
+
+function Checklist(){
+
+    return(
+        <div>
+            Hello World! hora di mata o corouna vairus
+        </div>
+    )
+
+}
+
+export default Checklist
