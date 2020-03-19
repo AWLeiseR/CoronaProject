3d417cf0a34e019c07a68bf34b0509b425d0145c
FernandoMPN <42014973+FernandoMPN@users.noreply.github.com>
2020-03-19 13:23:28 -0300

    Update MapUbs.js

--- src/MapUbs/MapUbs.js
+++ src/MapUbs/MapUbs.js
@@ -1,10 +1,9 @@
 import React,{useState,useEffect} from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import getUbsList from '../utils/fetchUBS'
+
+import getUBSList from '../utils/fetchUBS';
 
 const aux={lat:-23.319088,lng :-51.157803}
-
-let ubsList=[]
 
 function MapUbs(){
 
@@ -13,39 +12,30 @@ function MapUbs(){
         lng: -51.157803,
         zoom: 13,
     }
+
     const [latMarker,onchangelatMarker]=useState(-23.319088)
     const [longMarker,onchangelongMarker]=useState(-51.157803)
     const position = [mapa.lat, mapa.lng]
     const marker=[latMarker,longMarker]
     const [ubsList,onchangeUbs]=[]
     
-
     const updatePos=(event)=>{
       let aux=event.target.getLatLng()
       onchangelatMarker(aux.lat)
       onchangelongMarker(aux.lng)
     }
 
-    const displayUbs=()=>{
-      if(ubsList!=undefined){
-       return( ubsList.map(item=>(<Marker position={[item.lat,item.lon]}><Popup>
-          
-        </Popup></Marker>)))
-      }
-    }
-
     return (
         
         <Map center={position} zoom={mapa.zoom} style={{height:"80%", width: "100%"}}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {displayUbs()}
-        <Marker position={marker} draggable={true} onDragend={updatePos}>
-          <Popup>
-          
-          </Popup>
+            <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={marker} draggable={true} onDragend={updatePos}>
+            <Popup>
+            
+            </Popup>
         </Marker>
         
       </Map>
@@ -54,25 +44,31 @@ function MapUbs(){
 
 }
 
-function pageMap(){
-  
+function MapPage(){
 
-  const searchUbs=async ()=>{
+    const [ubsList, setUbsList] = useState([])
+    
+    const searchUBS = () => {
 
-    ubsList=await getUbsList(aux.lat,aux.lng)
-    console.log(ubsList)
-  }
+        getUBSList(aux.lat, aux.lng).then(data => {
+            setUbsList(data)
+        })
 
-  return(
-    <div style={{height:'100%'}}>
-      <MapUbs/>
-      <div onClick={searchUbs} style={{background:'#C34A3D',borderRadius:'10px',width:'130px',}}>
-      <p>Pesquisar ubs</p>
-      {ubsList.map(item=>(<p>ubd-> {item.display_name}</p>))}
-      </div>
-      
-    </div>
-  )
+    }
+
+    return(
+        <div style={{height:'100%'}}>
+
+            <MapUbs/> 
+            <div onClick={searchUBS} style={{background:'#C34A3D',borderRadius:'10px',width:'130px',}}>
+                <p>Pesquisar ubs</p>
+            </div>
+
+            {ubsList.map(item => {
+                return <p>ubs-> {item.display_name}</p>
+            })}
+        </div>
+    )
 }
 
-export default pageMap
+export default MapPage
