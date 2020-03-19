673a09e9a9bf63f5d80e7f6c949bce5b2b0b9691
LeiseR <willy.leiser@gmail.com>
2020-03-19 10:38:44 -0300

    fetch ubs

--- src/MapUbs/MapUbs.js
+++ src/MapUbs/MapUbs.js
@@ -1,6 +1,10 @@
-import React,{useState,createRef} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import getUbsList from '../utils/fetchUBS'
+
+const aux={lat:-23.319088,lng :-51.157803}
+
+let ubsList=[]
 
 function MapUbs(){
 
@@ -13,31 +17,62 @@ function MapUbs(){
     const [longMarker,onchangelongMarker]=useState(-51.157803)
     const position = [mapa.lat, mapa.lng]
     const marker=[latMarker,longMarker]
-
+    const [ubsList,onchangeUbs]=[]
     
 
     const updatePos=(event)=>{
       let aux=event.target.getLatLng()
       onchangelatMarker(aux.lat)
       onchangelongMarker(aux.lng)
-      //console.log(getUbsList(aux.lat,aux.long))
+    }
+
+    const displayUbs=()=>{
+      if(ubsList!=undefined){
+       return( ubsList.map(item=>(<Marker position={[item.lat,item.lon]}><Popup>
+          
+        </Popup></Marker>)))
+      }
     }
 
     return (
         
-        <Map center={position} zoom={mapa.zoom} style={{height:"100%", width: "100%"}}>
+        <Map center={position} zoom={mapa.zoom} style={{height:"80%", width: "100%"}}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {displayUbs()}
         <Marker position={marker} draggable={true} onDragend={updatePos}>
           <Popup>
-            Ubs here!
+          
           </Popup>
         </Marker>
+        
       </Map>
+      
     )
 
 }
 
-export default MapUbs
+function pageMap(){
+  
+
+  const searchUbs=async ()=>{
+
+    ubsList=await getUbsList(aux.lat,aux.lng)
+    console.log(ubsList)
+  }
+
+  return(
+    <div style={{height:'100%'}}>
+      <MapUbs/>
+      <div onClick={searchUbs} style={{background:'#C34A3D',borderRadius:'10px',width:'130px',}}>
+      <p>Pesquisar ubs</p>
+      {ubsList.map(item=>(<p>ubd-> {item.display_name}</p>))}
+      </div>
+      
+    </div>
+  )
+}
+
+export default pageMap
