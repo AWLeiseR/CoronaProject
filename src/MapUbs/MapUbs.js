3e68b1a36df05bb8daf905c829ba46f15a92fe48
LeiseR <willy.leiser@gmail.com>
2020-03-19 08:29:10 -0300

    draggable marker

--- src/MapUbs/MapUbs.js
+++ src/MapUbs/MapUbs.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React,{useState,createRef} from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import getUbsList from '../utils/fetchUBS'
 
 function MapUbs(){
 
     const mapa = {
-        lat: 51.505,
-        lng: -0.09,
+        lat:-23.319088,
+        lng: -51.157803,
         zoom: 13,
     }
-
+    const [latMarker,onchangelatMarker]=useState(-23.319088)
+    const [longMarker,onchangelongMarker]=useState(-51.157803)
     const position = [mapa.lat, mapa.lng]
+    const marker=[latMarker,longMarker]
+
+    
+
+    const updatePos=(event)=>{
+      let aux=event.target.getLatLng()
+      onchangelatMarker(aux.lat)
+      onchangelongMarker(aux.lng)
+      //console.log(getUbsList(aux.lat,aux.long))
+    }
 
     return (
         
@@ -18,9 +30,9 @@ function MapUbs(){
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={marker} draggable={true} onDragend={updatePos}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Ubs here!
           </Popup>
         </Marker>
       </Map>
